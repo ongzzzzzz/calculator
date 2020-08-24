8d2005cc6c0827328a10fbf55c2643ae924643a8
Ong Zhi Zheng <ongzhizheng@gmail.com>
2020-08-24 11:17:16 +0000

    saved, added jquery

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-var shift = document.getElementById("shift");
+$(document).ready(() => {
 
-shift.onclick = () => {
-  console.log("clicked")
-}
+  //toggle shift
+  $("#shift").click(() => {
+    if( $(".shifted").css("display") == "none"){
+      $(".unshifted").hide(); $(".shifted").show();
+    } else{
+      $(".unshifted").show(); $(".shifted").hide();
+    }
+    console.log("toggled shift");
+  });
+
+  //render display
+  // $(".unshifted").click(() => {
+    
+  //   console.log( $(this).html() );
+  // });
+
+
+});
+
+// register button clicks
+//display button clicks to display
+//parse numbers as ints so can do calc
+// differentiate between  +-*/ when click, show on display
+//when press = then update the display
+
+// (ints, functions, and syntaxes (decimal, comma, semicolon)) *****ints more importants
+//find an api which does derivat and integra, write a function for mixed fraction and sexagecimal
