263fd519938173c4a39d7e021bc2a15ce25b3640
Ong Zhi Zheng <ongzhizheng@gmail.com>
2020-08-30 05:54:49 +0000

    saved this mf

--- script.js
+++ script.js
@@ -1,23 +1,53 @@
-$(document).ready(() => {
+// $(document).ready(() => {
+//   //toggle shift
+//   $("#shift").click(() => {
+//     if( $(".shifted").css("display") == "none"){
+//       $(".unshifted").hide(); $(".shifted").show();
+//     } else{
+//       $(".unshifted").show(); $(".shifted").hide();
+//     }
+//     console.log("toggled shift");
+//   });
+//   //render display
+//   $(".button").click(() => {
+//     var self = $(this);
+//     console.log( self.html() );
+//   });
+// });
 
-  //toggle shift
-  $("#shift").click(() => {
-    if( $(".shifted").css("display") == "none"){
-      $(".unshifted").hide(); $(".shifted").show();
-    } else{
-      $(".unshifted").show(); $(".shifted").hide();
+function shiftButtons(){
+  var shifteds = document.getElementsByClassName("shifted");
+  var unshifteds = document.getElementsByClassName("unshifted");
+  for(var i = 0; i < shifteds.length; i++) {
+    if(shifteds[i].style.display == "none"){
+      shifteds[i].setAttribute("style", "display: inline");
+      unshifteds[i].setAttribute("style", "display: none");
+    }else{
+      shifteds[i].setAttribute("style", "display: none");
+      unshifteds[i].setAttribute("style", "display: inline");
     }
-    console.log("toggled shift");
+    console.log("woohoo");
+  }
+}
+
+function updateResult(element){
+  console.log(element.id);
+}
+
+var buttons = document.getElementsByClassName("button");
+for (var i = 0; i < buttons.length; i++) {
+  buttons.item(i).addEventListener("click", () => {
+    updateResult(this).bind(this);
   });
+}
+//this place cacat
+//maybe make a function called update result, then bind it to all of the buttons
 
-  //render display
-  // $(".unshifted").click(() => {
-    
-  //   console.log( $(this).html() );
-  // });
+//have a variable called result
+//defaults to 0
+//when 
 
 
-});
 
 // register button clicks
 //display button clicks to display
